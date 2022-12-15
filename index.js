71182a9ffa536a9937c8d26843685cc1faab71c8
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2022-12-15 17:30:30 +0300

    commit tree 39bdda8d6f4ac38b96d829671551ae0fd9ba03b9

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+
+//const input = document.getElementById('input');
+//input.addEventListener('click',addingEventListener);
+/*
+const = document.querySelector('#input').addEventListener('click', addingEventListener);
 function addingEventListener() {
+  alert('I was clicked!');
+};
+
+input.addEventListener('click', clickAlert);
+*/
+
+const input = document.getElementById('input');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
+
+
+
+function addingEventListener() {
+  alert('I was clicked!');
 }
+
+input.addEventListener('click', addingEventListener);
